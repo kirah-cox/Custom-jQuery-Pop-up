a14df59b475eb3e792f1bd05806abb402fa04d68
Kirah Cox <kirahanncox@gmail.com>
2025-09-27 23:43:27 -0600

    finished assignment

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,49 @@
+﻿
+
+function DialogBox(){
+    let formText = $('#addinformation').val();
+
+    if(formText.trim() === 'opensesame'){
+        $(function() {
+            $('#dialogsuccess').dialog('open');
+        });
+        setTimeout(function() {
+            $('#dialogsuccess').dialog('close');
+        }, 3000);
+    }
+    else{
+        $(function() {
+            $('#dialogfailure').dialog('open');
+        });
+        setTimeout(function() {
+            $('#dialogfailure').dialog('close');
+        }, 3000);
+    }
+
+    $('#addinformation').val('');
+}
+
+$(function() {
+    $('#dialogsuccess').dialog({ autoOpen: false });
+});
+
+$(function() {
+    $('#dialogfailure').dialog({ autoOpen: false });
+});
+
+$('#mybutton').on('click', function() {
+    $(document).ready(function(){
+        DialogBox();
+    })
+});
+
+$('#addinformationform').keypress(function(e) {
+    var code = e.keyCode || e.which;
+    if (code === 13) { 
+        $(document).ready(function(){
+            DialogBox();
+        })
+        
+        e.preventDefault();
+    }
+});
